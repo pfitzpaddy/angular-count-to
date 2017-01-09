eef86903ef0dfc1f0b052afd54ea712c95b69263
pfitzpaddy <gis.patrick@gmail.com>
2017-01-09 13:20:54 +0430

    npm/bower to v0.0.9

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'src/angular-count-to.js',
+                src: 'src/angular-filter-count-to.js',
                 dest: 'dist/<%= pkg.name %>.min.js'
             }
         },
